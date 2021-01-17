629ab1ad6785d06d7529510734c0eeaa52b0b9cd
Gusarov2906 <gusarov2906@gmail.com>
2021-01-17 12:59:47 +0300

    Created FXML Window

---
+++ src/Main/Controller.java
@@ -0,0 +1,82 @@
+package Main;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToolBar;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+
+public class Controller {
+
+    @FXML
+    private AnchorPane MainWindowId;
+
+    @FXML
+    private ToolBar ToolbarId;
+
+    @FXML
+    private MenuButton MenuButtonId;
+
+    @FXML
+    private MenuItem OpenDataButtonId;
+
+    @FXML
+    private MenuItem LoadChainButtonId;
+
+    @FXML
+    private MenuItem SaveChainButtonId;
+
+    @FXML
+    private GridPane GridPaneId;
+
+    @FXML
+    private ListView<?> List0Id;
+
+    @FXML
+    private ListView<?> List1Id;
+
+    @FXML
+    private ListView<?> List2Id;
+
+    @FXML
+    private ListView<?> List3Id;
+
+    @FXML
+    private ListView<?> List4Id;
+
+    @FXML
+    private ListView<?> List5Id;
+
+    @FXML
+    private ListView<?> List6Id;
+
+    @FXML
+    private ListView<?> List7Id;
+
+    @FXML
+    private TextField Filter0Id;
+
+    @FXML
+    private TextField Filter1Id;
+
+    @FXML
+    private TextField Filter2Id;
+
+    @FXML
+    private TextField Filter3Id;
+
+    @FXML
+    private TextField Filter4Id;
+
+    @FXML
+    private TextField Filter5Id;
+
+    @FXML
+    private TextField Filter6Id;
+
+    @FXML
+    private TextField Filter7Id;
+
+}
